2aeaa2c9d1dc017a8b35159d65757604e9a7c821
Tomas Hipolito <tlhipolito67@gmail.com>
2017-10-11 15:26:28 -0500

    -added truncate

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,9 +1,9 @@
 USE codeup_test_db;
 SELECT 'Albums released after 1991';
-SELECT * FROM albums WHERE release_date < 1991;
+DELETE * FROM albums WHERE release_date < 1991;
 
 SELECT 'Albums with the genre ''disco''';
-SELECT * FROM albums where genre = 'disco';
+DELETE * FROM albums where genre = 'disco';
 
 SELECT 'Albums by ''Whitney Houston'' ';
-SELECT * FROM albums where artist = 'Whitney Houston';
+DELETE * FROM albums where artist = 'Whitney Houston';
