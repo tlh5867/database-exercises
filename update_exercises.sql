3d08d9afe8df828a3cc26380fdaf9d0e32739e3c
Tomas Hipolito <tlhipolito67@gmail.com>
2017-10-11 14:06:24 -0500

    -added the quaries for the first half of the assignment

--- update_exercises.sql
+++ update_exercises.sql
@@ -3,15 +3,23 @@ use codeup_test_db;
 
 SELECT *
 from albums;
+UPDATE albums set sales = (sales * 10);
 
-SELECT 'All albums released before 1980' AS '';
-SELECT *
-FROM albums
-WHERE release_date > 1980;
 
-SELECT 'All albums by Michael Jackson' AS '';
-SELECT *
-FROM albums
-WHERE artist = 'Michael Jackson';
+SELECT * FROM albums WHERE release_date < 1980;
+UPDATE albums set release_date = (release_date - 100) WHERE release_date < 1980;
+
+
+
+SELECT * FROM albums WHERE artist = 'Michael Jackson';
+UPDATE albums SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
+
+
+
+
+
+
+
+
 
 
