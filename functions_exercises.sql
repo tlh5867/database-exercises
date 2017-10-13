ec8cf09b0cba07cd1868ea9b0bdc79717e5d4032
Tomas Hipolito <tlhipolito67@gmail.com>
2017-10-13 10:43:36 -0500

    -added the correct quaries to the functions exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -6,66 +6,24 @@ FROM employees
 WHERE first_name IN ('Irena','Vidya','Maya')
 GROUP BY gender;
 
-SELECT
-  gender,
-  first_name,
-  last_name
-FROM employees
-WHERE (first_name = 'Irena'
-       OR first_name = 'Vidya'
-       OR first_name = 'Maya')
-      AND gender = 'M';
-
-
 # Update your queries for employees whose names start and end with 'E'. Use concat() to combine their first and last name together as a single column in your results.
-SELECT CONCAT (first_name, '',last_name)
+SELECT CONCAT (first_name, ' ',last_name)
 from employees
 where last_name like 'E%'
 AND last_name like '%E'
 order by emp_no DESC;
 
-SELECT last_name
-FROM employees
-WHERE last_name LIKE 'e%'
-ORDER BY emp_no DESC;
-
-SELECT last_name
-FROM employees
-WHERE last_name LIKE 'e%' OR last_name LIKE '%e'
-ORDER BY emp_no DESC;
-
-SELECT last_name
-FROM employees
-WHERE last_name LIKE 'e%' AND last_name LIKE '%e'
-ORDER BY emp_no DESC;
+# For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company (Hint: You will also need to use now() or curdate())
 
 SELECT
-  hire_date,
-  first_name
-FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
-
-SELECT
+  datediff(curdate(),hire_date),
   hire_date,
   first_name
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
-      AND birth_date LIKE '%-12-25'
-ORDER BY birth_date, hire_date DESC;
-
-SELECT
-  hire_date,
-  first_name
-FROM employees
-WHERE hire_date LIKE '199%';
-
-# For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company (Hint: You will also need to use now() or curdate())
-
-SELECT
-  birth_date,
-  first_name
-FROM employees
-WHERE birth_date LIKE '%-12-25';
+And birth_date LIKE '%-12-25'
+GROUP BY 'namee_count', last_name, first_name
+ORDER BY name_count DESC ;
 
 SELECT last_name
 FROM employees
@@ -73,7 +31,11 @@ WHERE last_name LIKE '%q%';
 
 
 # Add a GROUP BY clause to your query for last names with 'q' and not 'qu' to find the distinct combination of first and last names. You will find there were some duplicate first and last name pairs in your previous results. Add a count() to your results and use ORDER BY to make it easier to find employees whose unusual name is shared with others.
-SELECT last_name
+SELECT count(*),
+first_name,
+  last_name
 FROM employees
 WHERE last_name LIKE '%q%'
-      AND last_name NOT LIKE '%qu%';
+      AND last_name NOT LIKE '%qu%'
+  GROUP BY first_name, last_name
+ORDER BY count(*) DESC;
